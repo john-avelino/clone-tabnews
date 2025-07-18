373b4c5386cfd9ecc762366cf0bbbb7cc7834d6b
john-avelino <john.avelino.jesus@gmail.com>
2025-07-18 01:52:38 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>John Avelino de Jesus ama mamãe e papai muito,se voces me amam ,da um sorriso</h1>
+    return <h1>John Avelino de Jesus ama mamãe e papai muito,se voces me amam ,da um sorriso ❤️❤️</h1>
 }
 
 export default Home;
