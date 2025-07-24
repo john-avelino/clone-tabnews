fc68fabefbd91d55062b31c3253f1c9ae6726eaf
john-avelino <john.avelino.jesus@gmail.com>
2025-07-24 01:40:15 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 meu primeiro repositório
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-    return <h1>John Avelino de Jesus ama mamãe e papai muito,se voces me amam ,da um sorriso ❤️❤️</h1>
+  return (
+    <h1>
+      John Avelino de Jesus ama mamãe e papai muito,se voces me amam ,da um
+      sorriso ❤️❤️
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
